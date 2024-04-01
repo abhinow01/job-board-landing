5833c9e11bb0839329cba0904eb9b757f485f4eb
abhinav khanna <kabhinav99@gmail.com>
2024-04-01 19:41:00 +0530

    initial commit

---
+++ app/navbar/page.tsx
@@ -0,0 +1,14 @@
+import ActionButtons from "./action-buttons";
+import Logo from "./logo";
+import NavigationBar from "./navigation-bar";
+
+const Navbar = ()=>{
+    return (
+        <div className="flex justify-between items-center border-b px-10 h-40 ">
+        <Logo/>
+        <NavigationBar/>
+        <ActionButtons/>
+        </div>
+    )
+}
+export default Navbar;
