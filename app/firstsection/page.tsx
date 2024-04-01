5e58678094d54861543158d7b11fde4d9c03746a
abhinav khanna <kabhinav99@gmail.com>
2024-04-01 23:39:21 +0530

    initial commit

---
+++ app/firstsection/page.tsx
@@ -0,0 +1,17 @@
+const FirstSection = () => {
+    return (
+      <section className="md:py-20 py-10 bg-gradient-to-r from-gray-100 to-gray-200 space-y-10">
+        <div className="container mx-auto text-center">
+          <div className="text-6xl font-bold md:px-20 pb-10 text-gradient bg-gradient-to-r from-blue-500 to-green-300 bg-clip-text text-transparent">
+            Hire Tech Talent 10x faster
+          </div>
+          <p className="text-lg md:text-xl md:px-10 bg-gradient-to-r from-black to-gray-400 bg-clip-text text-transparent font-bold">
+            Sourcing, Reviewing, and Shortlisting candidates made 10x faster
+          </p>
+        </div>
+      </section>
+    );
+  };
+  
+  export default FirstSection;
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./navbar/page";
+import FirstSection from "./firstsection/page";
 
 export default function Home() {
   return (
     <>
    <Navbar/>
+   <FirstSection/>
     </>
   );
 }
